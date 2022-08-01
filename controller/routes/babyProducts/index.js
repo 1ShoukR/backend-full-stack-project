dbbc8acd3949de4b809816c4d1cab9ffc479726f
1ShoukR <rahminshoukoohi@gmail.com>
2022-08-01 15:26:11 -0400

    added console logs for req.session

--- controller/routes/babyProducts/index.js
+++ controller/routes/babyProducts/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { babyProducts, boujieeClothes, computerProducts, freeJunks, Order, user, } = require('../../../sequelize/models');
 
-
+// check login function
 const checkLogin = (req, res, next) => {
   // add this function to every routes page and have it redirect them to the login page html file
   if (req.session.user) {
@@ -12,7 +12,10 @@ const checkLogin = (req, res, next) => {
   }
 };
 
+
+// gets all baby products from database
 router.get("/all_your_baby_needs", checkLogin, async (req, res) =>{
+    console.log(req.session.user);
     const babyStuffToGet = await babyProducts.findAll()
     console.log(babyStuffToGet)
     res.render("babyProducts.html" ,{Locals: {
