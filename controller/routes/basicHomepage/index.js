b4987c0ec4e9a689af163cd38e2393a1b899424b
1ShoukR <rahminshoukoohi@gmail.com>
2022-08-01 15:27:00 -0400

    added locals variable for basic homepage
    added req.sessionStore.user so that front end guy can print the user's name

--- controller/routes/basicHomepage/index.js
+++ controller/routes/basicHomepage/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const {babyProducts,boujieeClothes,computerProducts,freeJunk,Order,user,} = require('../../../sequelize/models');
 
 
-
+// can render out the name of the user I.E: "Welcome *insert user from req.session.user"
 router.get("/", async (req, res) =>{
-    res.render("index.html")
+    res.render("index.html", {locals: {name: req.sessionStore.user}})
 })
 
 
