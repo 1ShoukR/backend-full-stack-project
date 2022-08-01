75bb08458aa14ef38633f7f29b27415da23d4710
1ShoukR <rahminshoukoohi@gmail.com>
2022-08-01 11:36:10 -0400

    added check Login to babyProducts route

--- controller/routes/babyProducts/index.js
+++ controller/routes/babyProducts/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const { babyProducts, boujieeClothes, computerProducts, freeJunks, Order, user, } = require('../../../sequelize/models');
 
 
-router.get("/all_your_baby_needs", async (req, res) =>{
+const checkLogin = (req, res, next) => {
+  // add this function to every routes page and have it redirect them to the login page html file
+  if (req.session.user) {
+    next();
+  } else {
+    res.render('createUser.html');
+  }
+};
+
+router.get("/all_your_baby_needs", checkLogin, async (req, res) =>{
     const babyStuffToGet = await babyProducts.findAll()
     console.log(babyStuffToGet)
     res.render("babyProducts.html" ,{Locals: {
